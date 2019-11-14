0aa51f648388f28cb25ecac8b2f4a226f685ab5f
Andrea Bisello <andrea.bi@2csolution.it>
2019-11-14 17:41:45 +0100

    for debugging purpose

---
+++ image.test.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer')
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+expect.extend({ toMatchImageSnapshot });
+
+describe('Google Test', () => {
+
+    test("Google Logo", async () => {
+        browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: {width: 1800, height: 900},
+            args: ["--start-maximized"],
+            devtools: true
+        })
+        page = await browser.newPage()
+        await page.goto("https://www.google.com", {
+            timeout: 20000,
+            waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
+        })
+        logo = await page.waitForSelector('#hplogo', {visible: true, timeout:30000})
+        const selectorScreenshot = await logo.screenshot()
+        expect(selectorScreenshot).toMatchImageSnapshot({
+            failureThreshold: 0,
+            failureThresholdType: "percent",
+            diffDirection: "vertical"
+        })
+        browser.close()
+
+    })
+
+})
