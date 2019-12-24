4fbac37ad04dfbf4403295c1cf9b9f3e6256afb2
Andrea Bisello <andrea.bi@2csolution.it>
2019-12-24 17:07:48 +0100

    tried https://www.npmjs.com/package/puppeteer-extra-plugin-stealth and using local chrome, nothing changed.

--- headless-differences.test.js
+++ headless-differences.test.js
@@ -1,5 +1,7 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 expect.extend({ toMatchImageSnapshot });
 
 describe('headless differences Test', () => {
@@ -7,12 +9,14 @@ describe('headless differences Test', () => {
     test("headless and not headless screenshot should to be the same", async () => {
         jest.setTimeout(15000)
         browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             defaultViewport: {width: 1800, height: 900},
             args: ["--start-maximized"],
-            devtools: false
+            devtools: false,
+            executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
         })
         page = await browser.newPage()
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36')
         await page.goto("https://legaltest.solutiondocondemand.com", {
             timeout: 20000,
             waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
