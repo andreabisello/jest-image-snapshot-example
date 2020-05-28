df3fbb958cab973872c5f289dc8809555f33c1da
Andrea Bisello <andrea.bi@2csolution.it>
2020-05-28 16:00:49 +0200

    updated test

--- headless-differences.test.js
+++ headless-differences.test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer-extra')
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
+//const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+//puppeteer.use(StealthPlugin())
 expect.extend({ toMatchImageSnapshot });
 
 describe('headless differences Test', () => {
@@ -9,14 +9,12 @@ describe('headless differences Test', () => {
     test("headless and not headless screenshot should to be the same", async () => {
         jest.setTimeout(15000)
         browser = await puppeteer.launch({
-            headless: true,
+            headless: false,
             defaultViewport: {width: 1800, height: 900},
             args: ["--start-maximized"],
-            devtools: false,
-            executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+            devtools: false
         })
         page = await browser.newPage()
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36')
         await page.goto("https://legaltest.solutiondocondemand.com", {
             timeout: 20000,
             waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
