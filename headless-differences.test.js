ffecfb4f495642f1d57ca20514fca37fdf63d709
Andrea Bisello <andrea.bi@2csolution.it>
2021-04-14 17:00:55 +0200

    added image comparison ; updated packages ; added dependencies

--- headless-differences.test.js
+++ headless-differences.test.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer-extra')
-
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+expect.extend({ toMatchImageSnapshot });
 describe('headless differences Test', () => {
-
     test("headless and not headless screenshot should to be the same", async () => {
         jest.setTimeout(60000)
         browser = await puppeteer.launch({
             headless: false,
             defaultViewport: {width: 1800, height: 900},
-            args: ["--start-maximized"],
+            args: ["--start-maximized", "--disable-lcd-text", "--font-render-hinting=none"],
             devtools: false
         })
         page = await browser.newPage()
@@ -17,16 +17,19 @@ describe('headless differences Test', () => {
         })
         await page.waitFor(5000)
         loginBox = await page.waitForSelector('#loginBox', {visible: true, timeout:30000})
-
         //removing time variant area
         await page.evaluate(() => {
             document.querySelector('#lblNow').style.visibility = "hidden"
         })
-
         await page.screenshot({
             path:"gui.png"
         })
-
+        const selectorScreenshot = await loginBox.screenshot()
+        expect(selectorScreenshot).toMatchImageSnapshot({
+            failureThreshold: 0,
+            failureThresholdType: "percent",
+            diffDirection: "vertical"
+        })
         await browser.close()
     })
 })
